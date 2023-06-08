9b13b4dce02c17d83f9334ac2c613cb12b2da979
thulisile98 <thulisile.ma98@gmail.com>
2023-06-08 16:29:16 +0200

    node js  and express tut

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello Thuly');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3]);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.params.id);
+});
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => console.log(`listening on port ${port}`));
